2f7724e5d84bbfdd7b2b7ae0094ca2098b266db0
Giovanni Tjahjono <giovannitjahjono@192-168-1-8.tpgi.com.au>
2020-03-18 13:38:39 +1100

    Initiate Landing Page

---
+++ Jastip/resources/views/bootstrap.blade.php
@@ -0,0 +1,9 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Shop Homepage - Start Bootstrap Template</title>
+  <!-- Bootstrap core CSS -->
+  <link href="{{ asset('bootstrap/bootstrap.css') }}" rel="stylesheet">
+</head>
---
+++ Jastip/resources/views/components/sidebar.blade.php
@@ -0,0 +1,18 @@
+@section('sidebar')
+<div class="card my-4">
+    <h5 class="card-header">Search</h5>
+    <div class="card-body">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+                <button class="btn btn-secondary" type="button">Go!</button>
+            </span>
+        </div>
+    </div>
+</div>
+<div class="list-group">
+    <a href="#" class="list-group-item">Food and Drink</a>
+    <a href="#" class="list-group-item">Clothes</a>
+    <a href="#" class="list-group-item">Book</a>
+</div>
+@endsection
