0d9774ca3ff5bcd442a25d731f4ba9db7ae32d2b
galihsamoo <galihsamoo07@gmail.com>
2020-04-21 18:42:44 +0700

    Updating Catatan

---
+++ Jastip/app/Http/Controllers/PesanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pesan;
+use Illuminate\Http\Request;
+
+class PesanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pesan  $pesan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pesan $pesan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pesan  $pesan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pesan $pesan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pesan  $pesan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pesan $pesan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pesan  $pesan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pesan $pesan)
+    {
+        //
+    }
+}
