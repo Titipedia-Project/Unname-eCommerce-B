6c54e82fbbb4e64b2f21ea181cccdb22dbbaef20
Giovanni Tjahjono <ivangiovn@gmail.com>
2020-05-27 14:45:17 +0800

    Adding Bulk Buy Migration, Controller, and Model

---
+++ Jastip/app/Http/Controllers/ProductBulkBuyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProductBulkBuy;
+use Illuminate\Http\Request;
+
+class ProductBulkBuyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductBulkBuy  $productBulkBuy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductBulkBuy $productBulkBuy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductBulkBuy  $productBulkBuy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductBulkBuy $productBulkBuy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductBulkBuy  $productBulkBuy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductBulkBuy $productBulkBuy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductBulkBuy  $productBulkBuy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductBulkBuy $productBulkBuy)
+    {
+        //
+    }
+}
