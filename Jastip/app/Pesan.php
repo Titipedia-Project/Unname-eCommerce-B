a8482f0bc2ea412c5517d85fa1d1c008fc646b6e
galihsamoo <galihsamoo07@gmail.com>
2020-05-30 21:59:05 +0700

    Updateing pesan minus redirect

--- Jastip/app/Pesan.php
+++ Jastip/app/Pesan.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Pesan extends Model
 {
     //
+    protected $fillable = ['isi_pesan', 'id_pengirim', 'id_penerima', 'waktu_kirim', 'dibaca'];
 }
--- Jastip/resources/views/pages/message/pesan.blade.php
+++ Jastip/resources/views/pages/message/pesan.blade.php
@@ -23,78 +23,61 @@
                                 <h3 class="box-title">Direct Chat</h3>
 
                                 <div class="box-tools pull-right">
-                                    <span data-toggle="tooltip" title="3 New Messages" class="badge bg-light-blue">3</span>
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-box-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle">
-                                        <i class="fa fa-comments"></i></button>
-                                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+
                                 </div>
                             </div>
                             <!-- /.box-header -->
                             <div class="box-body">
                                 <!-- Conversations are loaded here -->
                                 <div class="direct-chat-messages">
+                                    @foreach($pesan as $data)
+                                    <!-- Message. Default to the left -->
+                                    @if ($data->id_pengirim === Auth::user()->id)
                                     <!-- Message. Default to the left -->
                                     <div class="direct-chat-msg">
                                         <div class="direct-chat-info clearfix">
-                                            <span class="direct-chat-name pull-left">Alexander Pierce</span>
-                                            <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+                                            <span class="direct-chat-name pull-left"><?= $user1->first()->name; ?></span>
+                                            <span class="direct-chat-timestamp pull-right">{{$data->waktu_kirim}}</span>
                                         </div>
                                         <!-- /.direct-chat-info -->
                                         <img class="direct-chat-img" src="{{ asset('photo_profile/'.Auth::user()->foto)}}" alt="Message User Image"><!-- /.direct-chat-img -->
                                         <div class="direct-chat-text">
-                                            Is this template really for free? That's unbelievable!
+                                            {{$data->isi_pesan}}
                                         </div>
+
                                         <!-- /.direct-chat-text -->
                                     </div>
                                     <!-- /.direct-chat-msg -->
-
+                                    @else
                                     <!-- Message to the right -->
                                     <div class="direct-chat-msg right">
                                         <div class="direct-chat-info clearfix">
-                                            <span class="direct-chat-name pull-right">Sarah Bullock</span>
-                                            <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
+                                            <span class="direct-chat-name pull-right"><?= $user2->first()->name; ?></span>
+                                            <span class="direct-chat-timestamp pull-left">{{$data->waktu_kirim}}</span>
                                         </div>
                                         <!-- /.direct-chat-info -->
-                                        <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
+                                        <img class="direct-chat-img" src="{{ asset('photo_profile/'.Auth::user()->foto)}}" alt="Message User Image"><!-- /.direct-chat-img -->
                                         <div class="direct-chat-text">
-                                            You better believe it!
+                                            {{$data->isi_pesan}}
                                         </div>
                                         <!-- /.direct-chat-text -->
                                     </div>
+                                    @endif
                                     <!-- /.direct-chat-msg -->
+                                    @endforeach
                                 </div>
                                 <!--/.direct-chat-messages-->
 
-                                <!-- Contacts are loaded here -->
-                                <div class="direct-chat-contacts">
-                                    <ul class="contacts-list">
-                                        <li>
-                                            <a href="#">
-                                                <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg" alt="User Image">
 
-                                                <div class="contacts-list-info">
-                                                    <span class="contacts-list-name">
-                                                        Count Dracula
-                                                        <small class="contacts-list-date pull-right">2/28/2015</small>
-                                                    </span>
-                                                    <span class="contacts-list-msg">How have you been? I was...</span>
-                                                </div>
-                                                <!-- /.contacts-list-info -->
-                                            </a>
-                                        </li>
-                                        <!-- End Contact Item -->
-                                    </ul>
-                                    <!-- /.contatcts-list -->
-                                </div>
                                 <!-- /.direct-chat-pane -->
                             </div>
                             <!-- /.box-body -->
                             <div class="box-footer">
-                                <form action="#" method="post">
+                                <form action="/kirim" enctype="multipart/form-data" method="post">
+                                    @csrf
                                     <div class="input-group">
-                                        <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+                                        <input type="text" name="isi_pesan" placeholder="Type Message ..." class="form-control" required>
+                                        <input type="hidden" name="id_penerima" value="{{$user2->first()->id}}" />
                                         <span class="input-group-btn">
                                             <button type="submit" class="btn btn-primary btn-flat">Send</button>
                                         </span>
